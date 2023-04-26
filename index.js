3f72bc72f79cfe012e630f5ba4aa84f6d047addf
Runar <runarnazmutdinov@gmail.com>
2023-04-26 11:33:50 +0300

    fix: cors

--- index.js
+++ index.js
@@ -16,7 +16,11 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 // Что бы обойти cors
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tehno-store-frontend.vercel.app/",
+  })
+);
 
 // Формат json для Express
 app.use(express.json());
