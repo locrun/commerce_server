363767952e6299adac37676b604d7526589e8527
Runar <runarnazmutdinov@gmail.com>
2023-05-01 15:33:05 +0300

    update useRouter.js

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -5,19 +5,19 @@ import authMiddleware from "../middleware/authMiddleware.js";
 
 const router = new Router();
 
-router.post("/registration", UserController.registration, () => {
+router.post("/registration", UserController.registration, (req, res) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
     "https://tehno-store.vercel.app"
   );
 });
-router.post("/login", UserController.login, () => {
+router.post("/login", UserController.login, (req, res) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
     "https://tehno-store.vercel.app"
   );
 });
-router.get("/check", authMiddleware, UserController.check, () => {
+router.get("/check", authMiddleware, UserController.check, (req, res) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
     "https://tehno-store.vercel.app"
