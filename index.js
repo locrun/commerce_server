26d3ed77be7173bed6d8e0df16bfb0a56deef57b
Runar <runarnazmutdinov@gmail.com>
2023-05-01 11:30:10 +0300

    update cors

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const app = express();
 // Cross-Origin Resource Sharing
 app.use(
   cors({
-    origin: "https://tehno-store-frontend.vercel.app",
+    origin: "https://tehno-store.vercel.app",
     credentials: true,
     methods: ["GET", "PUT", "POST", "DELETE"],
   })
